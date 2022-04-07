71961c5fc780aa8603c0313c19dd2ecb3dbcd6df
wkuna <alessandro.curzi@gmail.com>
2022-04-07 16:15:16 +0100

    First plugin draft

--- index.js
+++ index.js
@@ -19,180 +19,193 @@ const Bacon = require('baconjs');
 const fs = require('fs');
 const async = require('async');
 
+// These values were generated using calibrate_mag.js - you will want to create your own.
+var MAG_CALIBRATION = {
+  min: { x: -36.44921875, y: -13.0625, z: -102.65625 },
+  max: { x: 112.875, y: 129.4375, z: 62.734375 },
+  offset: { x: 38.212890625, y: 58.1875, z: -19.9609375 },
+  scale:
+  {
+    x: 1.5309467130562169,
+    y: 1.6042626096491228,
+    z: 1.3822272083136513
+  }
+}
 
-module.exports = function(app) {
-  var BNO055;
-  if ( fs.existsSync('/sys/class/i2c-adapter') ) { 
+// These values were generated using calibrate_gyro.js - you will want to create your own.
+// NOTE: These are temperature dependent.
+var GYRO_OFFSET = { x: 0.2880610687022899,
+  y: -0.06345038167938938,
+  z: 0.11358778625954198 };
+
+// These values were generated using calibrate_accel.js - you will want to create your own.
+var ACCEL_CALIBRATION = {
+  offset: {
+    x: 0.00943176,
+    y: 0.00170817,
+    z: 0.05296142
+  },
+  scale: {
+    x: [-0.9931640, 1.0102189],
+    y: [-0.9981974, 1.0055884],
+    z: [-0.9598844, 1.0665967]
+  }
+};
+
+
+
+module.exports = function (app) {
+  var motionInterval;
+  var mpu9255;
+  if (fs.existsSync('/sys/class/i2c-adapter')) {
     // 1 wire is enabled a
-    BNO055 = require('node-bno055');
+    mpu9255 = require('mpu9255');
 
-    console.log("signalk-imu: BMO055 avaiable. ");
-  } else {
-    BNO055 = require('./fakesensor');
-    console.log("signalk-imu: BMO055 not available, Created Fake Sensor ");
+    console.log("signalk-imu: MPU9255 avaiable. ");
   }
 
   var plugin = {};
-  var unsubscribes = [];
 
-  function convertToK(v) {
-    return v+273.15;
+  function p(num) {
+    if (num === undefined) {
+      return '       ';
+    }
+    var str = num.toFixed(3);
+    while (str.length <= 7) {
+      str = ' ' + str;
+    }
+    return str + ' ';
   }
 
+  function calcHeading(x, y) {
+    var heading = Math.atan2(y, x);
 
-  plugin.start = function(config) {
+
+    return heading;
+  }
+
+  plugin.start = function (config) {
     // console.log("IMU Config ", JSON.stringify(config));
-   
-    var bno055 = new BNO055(config);
+    // Instantiate and initialize.
+    var mpu = new mpu9255({
+      // i2c path (default is '/dev/i2c-1')
+      device: '/dev/i2c-1',
 
-    var configStatus = undefined;
-    var calibrated = false;
-    var callSequence = {
-      checkCalibration: function(callback) {
-        if ( !calibrated ) {
-          bno055.getCalibrationStatus((err, res) =>{
-            if (err) return callback(err);
-            res.system  = "Ok";
-            res.gyro = "Ok";
-            res.mag = "Ok";
-            res.accel = "Ok";
-            res.cfgStatus = 0;
-            if ( res.systemStatus !== 0x03 ) {
-              res.system = "Needs calibration.";
-              res.cfgStatus = res.cfgStatus | 0x01;
-            }
-            if ( res.gyroStatus !== 0x03 ) {
-              res.gyro = "Gyro needs calibrating, keep sensor still for a few seconds.";
-              res.cfgStatus = res.cfgStatus | 0x02;
-            }
-            if ( res.accelerometerStatus !== 0x03 ) {
-              res.accel = "Accelerometer needs calibrating, Slowly move between 6 stable positions, and hold for > 2s in each.";
-              res.cfgStatus = res.cfgStatus | 0x04;
-            }
-            if ( res.magnetometerStatus !== 0x03 ) {
-              res.mag = "Magnatomiter needs calibrating, Perform figure of 8 movements.";
-              res.cfgStatus = res.cfgStatus | 0x08;
-            }
-            if ( res.cfgStatus !== 0 ) {
-              if (configStatus === undefined || configStatus.cfgStatus !== res.cfgStatus ){
-                console.log("BNO055 Calibration May be required. ", res);
-              }
-              configStatus = res;
-              callback(null, res);
-            } else {
-              configStatus = res;
-              calibrated = true;
-              console.log("BNO055 Calibration Ok.")
-              callback(null, res);
-            }
-          });
-        } else {
-          callback(null, configStatus);
-        }
-      },
-      temperature: function(callback) { bno055.getTemperature(callback)},
-      euler: function(callback) { bno055.getEuler(callback)},
-      laccel: function(callback) { bno055.getLinearAcceleration(callback)},
-      gyro: function(callback) { bno055.getGyroscope(callback)}
-    };
-    bno055.beginNDOF((err, startOk) => {
-      if ( err ) {
-        console.log("IMU Failed to start", err);
-      } else {
-        plugin.motionInterval = setInterval(() => {
-          async.series(callSequence, (err, res) => {
-            if (err) {
-              console.log("Failed to read IMU", err, res);
-            } else {
-              var delta = {
-                "context": "vessels." + app.selfId,
-                "updates": [
-                  {
-                    "source": {
-                      "src": "BNO055"
-                    },
-                    "timestamp": (new Date()).toISOString(),
-                    "values": [
-                        {
-                          "path" : "environment.inside.temperature",
-                          "value" : convertToK(res.temperature)
-                        },
-                        {
-                          "path": "navigation.rateOfTurn",
-                          "value": res.gyro.z
-                        },
-                        {
-                          "path": "navigation.gyro.roll",
-                          "value": res.gyro.x
-                        },
-                        {
-                          "path": "navigation.gyro.pitch",
-                          "value": res.gyro.y
-                        },
-                        {
-                          "path": "navigation.gyro.yaw",
-                          "value": res.gyro.z
-                        },
-                        {
-                          "path": "navigation.accel.x",
-                          "value": res.laccel.x
-                        },
-                        {
-                          "path": "navigation.accel.y",
-                          "value": res.laccel.y
-                        },
-                        {
-                          "path": "navigation.accel.z",
-                          "value": res.laccel.z
-                        },
-                        {
-                          "path": "navigation.headingMagnetic",
-                          "value": res.euler.heading,
-                        },
-                        {
-                          "path": "navigation.attitude.roll",
-                          "value": res.euler.roll,
-                        },
-                        {
-                          "path": "navigation.attitude.pitch",
-                          "value": res.euler.pitch,
-                        }
-                      ]
-                  }
-                ]
-              };
-              //console.log("signalk-imu got motion delta: " + JSON.stringify(delta))
-              app.handleMessage(plugin.id, delta);
-            }
-          });
-        
-        },  config.motionPeriod);
-      }
+      // Enable/Disable debug mode (default false)
+      DEBUG: true,
+
+      // Set the Gyroscope sensitivity (default 0), where:
+      //      0 => 250 degrees / second
+      //      1 => 500 degrees / second
+      //      2 => 1000 degrees / second
+      //      3 => 2000 degrees / second
+      GYRO_FS: 0,
+
+      // Set the Accelerometer sensitivity (default 2), where:
+      //      0 => +/- 2 g
+      //      1 => +/- 4 g
+      //      2 => +/- 8 g
+      //      3 => +/- 16 g
+      ACCEL_FS: 0,
+
+      scaleValues: true,
+
+      UpMagneto: true,
+
+      magCalibration: MAG_CALIBRATION,
+
+      gyroBiasOffset: GYRO_OFFSET,
+
+      accelCalibration: ACCEL_CALIBRATION
     });
 
+    console.log("Initializing ...");
+    var kalmanX = new mpu.Kalman_filter();
+    var kalmanY = new mpu.Kalman_filter();
 
-    
+    if (mpu.initialize()) {
+      motionInterval = setInterval(function () {
+        var values = mpu.getMotion9();
+        var end = new Date().getTime();
+
+        console.log(values)
+
+        var delta = {
+          "context": "vessels." + app.selfId,
+          "updates": [
+            {
+              "source": {
+                "src": "MPU9255"
+              },
+              "timestamp": end,
+              "values": [
+                {
+                  "path": "environment.inside.temperature",
+                  "value": mpu.getTemperatureCelsiusDigital()
+                },
+                {
+                  "path": "navigation.rateOfTurn",
+                  "value": values[5]
+                },
+                {
+                  "path": "navigation.gyro.roll",
+                  "value": values[3]
+                },
+                {
+                  "path": "navigation.gyro.pitch",
+                  "value": values[4]
+                },
+                {
+                  "path": "navigation.gyro.yaw",
+                  "value": values[5]
+                },
+                {
+                  "path": "navigation.accel.x",
+                  "value": values[0]
+                },
+                {
+                  "path": "navigation.accel.y",
+                  "value": values[1]
+                },
+                {
+                  "path": "navigation.accel.z",
+                  "value": values[2]
+                },
+                {
+                  "path": "navigation.headingMagnetic",
+                  "value": calcHeading(values[6], values[7]),
+                }
+              ]
+            }
+          ]
+        };
+        app.handleMessage(plugin.id, delta);
+      }, config.motionPeriod);
+    } else {
+      console.log("init returning false")
+    }
+    console.log(motionInterval);
   }
 
-  plugin.stop = function() {
-    if ( plugin.environmentInterval !== undefined ) {
+  plugin.stop = function () {
+    if (plugin.environmentInterval !== undefined) {
       clearInterval(plugin.environmentInterval);
     }
-    if ( plugin.motionInterval !== undefined ) {
-      clearInterval(plugin.motionInterval);
+    if (motionInterval !== undefined) {
+      clearInterval(motionInterval);
     }
   }
 
-  plugin.id = "sk-imu"
+  plugin.id = "mpu9255-imu"
   plugin.name = "IMU Source"
   plugin.description = "Plugin that reads IMU data"
 
   plugin.schema = {
     title: "IMU Source",
-    description: "This plugin reads data from a I2C attached BNO055 defice. The device should be set up so that the BNO055 on the chip is towards the bow.",
+    description: "This plugin reads data from a I2C attached MPU9255 device. The device should be set up so that the MPU9255 on the chip is towards the bow.",
     type: "object",
     properties: {
-      motionPeriod : {
+      motionPeriod: {
         title: "Period of motion readings in ms",
         type: "integer",
         default: 1000
@@ -200,11 +213,9 @@ module.exports = function(app) {
     }
   }
 
-
-
   plugin.uiSchema = {
     "ui:order": [
-    'motionPeriod'
+      'motionPeriod'
     ]
   };
 
